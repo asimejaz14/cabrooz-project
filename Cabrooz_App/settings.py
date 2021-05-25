167bbc6016e02f717447460f4cc3297131ef10df
Asim Ejaz <asim.ejaz14@gmail.com>
2021-05-26 01:21:20 +0500

    requirements updated

--- Cabrooz_App/settings.py
+++ Cabrooz_App/settings.py
@@ -148,11 +148,11 @@ REST_FRAMEWORK = {
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
